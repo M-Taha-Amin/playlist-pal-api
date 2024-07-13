a196640897a5c4ee834b15791d1adaf8e46f088b
M-Taha-Amin <mtahaamin.official@gmail.com>
2024-07-13 12:06:49 +0500

    removed unused package

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require('path');
 const routes = require('./routes/index');
 const cors = require('cors');
-require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
