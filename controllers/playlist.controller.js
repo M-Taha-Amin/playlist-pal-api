2b796965740ff876f7fcb1c6e050d2ab2188153b
taha <mtahaamin.official@gmail.com>
2024-08-20 14:43:48 +0500

    refactored code

---
+++ controllers/playlist.controller.js
@@ -0,0 +1,25 @@
+import { getVideoDuration } from '../lib/youtubeDataApi/getVideoDuration.js';
+import { getVideoIDs } from '../lib/youtubeDataApi/getVideoIDs.js';
+
+export const getPlaylistDuration = async (req, res) => {
+  const { apiKey, playlistId } = req.body;
+  if (!apiKey || !playlistId) {
+    res
+      .status(400)
+      .json({ error: 'Bad Request, api key or playlist id missing.' });
+    return;
+  }
+  try {
+    const playlistVideosIds = await getVideoIDs(apiKey, playlistId);
+    let playlistDuration = await Promise.all(
+      playlistVideosIds.map(
+        async videoId => await getVideoDuration(apiKey, videoId)
+      )
+    );
+    playlistDuration = playlistDuration.filter(duration => duration !== null);
+    res.status(200).json(playlistDuration);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import playlistRoutes from './routes/playlist.routes.js';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+const __dirname = path.resolve();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/playlist', playlistRoutes);
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on Port 3000...');
+});
